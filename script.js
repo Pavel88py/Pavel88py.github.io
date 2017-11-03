95a7036e31943190a8489b2a635c9d95ed71bf4e
Pavel88py <silber.ypm@gmail.com>
2017-11-03 08:06:39 +0500

    init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$('.carousel').carousel({
+
+	interval: 5000,				
+	keyboard: false,			//  keyboard buttons on when 'false' 
+	wrap: true 
+	}
+)
+
+// var val = $(":contains('Google')");
+// $("[http://www.google.com]").html("Yandex");
+
+
